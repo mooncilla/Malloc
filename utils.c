86c930d640b6434068280a1373e852fed8de276e
gastal_r <remi.gastaldi@epitech.eu>
2017-01-24 14:23:32 +0100

    :)

---
+++ utils.c
@@ -0,0 +1,30 @@
+/*
+** utils.c for utils in /home/gastal_r/rendu/PSU_2016_malloc
+** 
+** Made by 
+** Login   <gastal_r>
+** 
+** Started on  Tue Jan 24 14:19:22 2017 
+** Last update Tue Jan 24 14:23:03 2017 
+*/
+
+#include	"unistd.h"
+
+int		my_strlen(char *str)
+{
+  int	i;
+
+  i = -1;
+  while (str[++i]);
+  return (i);
+}
+
+void		my_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void		my_putstr(char *str)
+{
+  write (1, str, my_strlen(str));
+}
