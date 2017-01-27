63399b411f4fe7fcb8282571d58a4deaf7b24668
gastal_r <remi.gastaldi@epitech.eu>
2017-01-27 10:03:32 +0100

    segfault mdr

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 ** Login   <gastal_r>
 **
 ** Started on  Tue Jan 24 14:47:37 2017
-** Last update	Thu Jan 26 12:34:38 2017 Full Name
+** Last update	Fri Jan 27 09:55:22 2017 Full Name
 */
 
 #include	<unistd.h>
@@ -14,33 +14,15 @@
 
 #define   PAGESIZE sysconf(_SC_PAGESIZE)
 
-static    t_free      *freeStruct;
-static    t_malloc    *mallocStruct;
+static    t_malloc    *mallocStruct = NULL;
 
 void	check_double_empty()
 {
-  t_free	*tmp;
-  t_free	*tmp2;
-
-  tmp = freeStruct;
-  tmp2 = freeStruct;
-  while (tmp2)
+  /* t_free	;
+  while (freeStruct && freeStruct->next != NULL)
     {
-      freeStruct = tmp;
-      while (freeStruct->next != NULL)
-	{
-	  if (&freeStruct->next == (&freeStruct + freeStruct->size))
-	    {
-	      freeStruct->size += freeStruct->next->size;
-	      if (freeStruct->next->next != NULL)
-		freeStruct->next = freeStruct->next->next;
-	      else
-		freeStruct->next = NULL;
-	    }
-	  freeStruct = freeStruct->next;
-	}
-      tmp2 = tmp2->next;
-    }
+      if (&freeStruct == (&freeStruct + )
+    } */
 }
 
 void    my_free(void *ptr)
@@ -48,45 +30,91 @@ void    my_free(void *ptr)
   check_double_empty();
 }
 
-void		*check_in_free_list(unsigned int size)
+int		check_in_free_list(unsigned int size)
 {
-  t_free	*tmp; 
-  while (freeStruct)
+/*  while (freeStruct)
     {
       if (freeStruct->size >= size)
-	{
-	  tmp = freeStruct
-	  return(tmp);
-	}
+	return(1);
       freeStruct = freeStruct->next;
-    }
-  return (NULL);
+  } */
+  return (0);
 }
 
-void		my_malloc(unsigned int size)
+void    *push_back_malloc_list(unsigned int size)
 {
-  static int   mallocStructSize = 0;
-  static int   pagerUsedSize = 0;
+    if (mallocStruct == NULL)
+    {
+      //printf("Creation de la liste\n");
+      mallocStruct = sbrk(0) - 4096;
+      //printf("ptr = %p\n", mallocStruct);
+      mallocStruct->size = size;
+      mallocStruct->next = NULL;
+      mallocStruct->isFree = false;
+      return (mallocStruct + sizeof(t_malloc) + 1);
+    }
+    else
+    {
+      void *tmp = mallocStruct;
+      while (mallocStruct->next)
+        mallocStruct = mallocStruct->next;
+    //  //printf("avant= %s\n", mallocStruct + sizeof(t_malloc) + 1);
+      //printf("Nouveau maillon\n");
+      //printf("%d\n",mallocStruct->size + sizeof(t_malloc) + 1);
+      mallocStruct->next = mallocStruct + mallocStruct->size + sizeof(t_malloc) + 1;
+      mallocStruct->next->size = size;
+      mallocStruct->next->isFree = false;
+      mallocStruct->next->next = NULL;
+      mallocStruct = tmp;
+      return (mallocStruct->next + sizeof(t_malloc) + 1);
+    }
+}
+
+void		*my_malloc(unsigned int size)
+{
+  static int   pagerUsedSize = 4096;
   // TODO -> check_in_free_list(unsigned int size);
   // si pas de place dans la liste de free -> check si la taille dans le pager est suffisante
-  if (check_in_free_list(size) == 1)
+/*  if (check_in_free_list(size) == 1)
     {
     }
   else
-    {
+    { */
       if ((PAGESIZE - pagerUsedSize) < size)
-	void  *ptr = sbrk(PAGESIZE);
+      {
+	       void *ptr = sbrk(PAGESIZE);
+         //printf("%p\n", ptr);
+         pagerUsedSize = (size + sizeof(t_malloc)) - (PAGESIZE - pagerUsedSize);
+         //printf("New page %d\n", pagerUsedSize);
+         return (push_back_malloc_list(size));
+      }
       else
-	{
-	}
-    }
-  /* mallocStruct =  ptr;
-  mallocStructSize += sizeof(t_malloc);
-  printf("%d\n", mallocStructSize); */
+      {
+        pagerUsedSize += size + sizeof(t_malloc);
+        //printf("Existing page %d\n", pagerUsedSize);
+        return (push_back_malloc_list(size));
+      }
 }
 
+#include <string.h>
 
 int main()
 {
-  my_malloc(2);
+  /*oid *ptr1 = sbrk(10);
+   printf("bite\n");
+  void *ptr2 = sbrk(0) - 10;
+  printf("%p\n", ptr1);
+  printf("%p\n", ptr2);
+return(0); */
+  char *test = my_malloc(500);
+  test = strcpy(test, "bite");
+
+  char *bonsoir = my_malloc(200);
+  bonsoir = strcpy(bonsoir, "b");
+
+  while (mallocStruct)
+  {
+    printf("data= %s\n", mallocStruct + sizeof(t_malloc) + 1);
+    mallocStruct = mallocStruct->next;
+  }
 }
